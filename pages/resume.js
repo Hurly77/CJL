2f899d55ffe804cd2dcec7b8fda6248e28163317
Cameron Leverett <camrbo@gmail.com>
2021-11-28 23:51:09 -0800

    added download button for pdf

--- pages/resume.js
+++ pages/resume.js
@@ -2,9 +2,21 @@ import ResumeComponent from '../components/resume/index';
 
 const Resume = () => {
 	return (
-		<div className='flex items-center justify-center w-full pb-10'>
-			<ResumeComponent />
-		</div>
+		<>
+			<div className='flex flex-col items-center justify-center w-full pb-10'>
+				<div className="flex justify-center w-full max-w-4xl pb-5 md:justify-start">
+					<a
+						className='btn btn-primary'
+						href='/images/Cameron-Leverett-Resume.pdf'
+						download='Cameron-Leverett-Resume.pdf'>
+						{' '}
+						Download{' '}
+					</a>
+				</div>
+
+				<ResumeComponent />
+			</div>
+		</>
 	);
 };
 
